1969eec623ad9ba69718c290190bab234b1837ff
alyssonmill <alyssonmillerdev@gmail.com>
2024-05-27 12:53:09 +0100

    Adding localStorage, setItem and also getItem both in a function

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
-const inputs =  document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
+const inputDays = document.querySelectorAll('input[name="Availability"]')
 
 const userName = document.getElementById("user_name");
 const userAddress = document.getElementById("user_address");
+
+
 
 inputs.forEach((inputs) => {
     inputs.addEventListener("focus", (event) => {
         event.target.style.border = "3px solid purple";
     });
-    
+
     inputs.addEventListener("blur", (event) => {
         event.target.style.border = "";
+        if (inputs.type === "checkbox") {
+            window.localStorage.setItem(inputs.name, inputs.checked);
+        } else {
+            window.localStorage.setItem(inputs.name, inputs.value);
+        }
     });
-
-    userName.addEventListener("blur", () => {
-       window.sessionStorage.setItem("Name", userName.value)
-    });
-
-    userAddress.addEventListener("blur", () => {
-        window.sessionStorage.setItem("Address", userAddress.value)
-     });
 });
 
-const availability = document.getElementById("availability");
-const radioInputs = availability.querySelectorAll("input[type='radio']");
-
-function storage (){
-    radioInputs.forEach((radio) => {
-        radio.addEventListener("change", (event) =>{
-            if(event.target.checked){
-                window.localStorage.setItem(event.target.value ,event.target.value)
-            }
-        });
-    });
+function checkValue (){
+    inputs.forEach((inputs) => {
+        const value = window.localStorage.getItem(inputs.name)
+        if (inputs.type === "checkbox") {
+            inputs.checked = value === "true";
+        } else if (value) {
+            inputs.value = value;
+        }
+    })
 }
-radioInputs.forEach((radio) => {
-    radio.addEventListener("focus", storage);
-});
 
-
-
-
-
-// availability.addEventListener("blur", (event) => {
-//     if(event.target.checked){
-//         window.sessionStorage.setItem("Availability", availability.value)
-//     }else{
-//         console.log("Errorrr")
-//     }
-//  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Parte 2
-
-// Agora substitua todos os comandos de logs por comandos para salvar mecanismo localStorage para que seja possível recuperar ao pressionar F5
-// Crie uma função que recupera os valores do localStorage e preenche os inputs corretamente (Dicas: input.value = “valor” e <script> no final do body)
-
-
-
-
+document.addEventListener('DOMContentLoaded', checkValue);
