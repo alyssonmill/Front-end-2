f8c15fcfbf694bc0b03704c00e074ef2394d2c06
alyssonmill <alyssonmillerdev@gmail.com>
2024-05-28 10:53:59 +0100

    Ending front-end-2

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const inputs = document.querySelectorAll("input");
 const inputDays = document.querySelectorAll('input[name="Availability"]')
 
-const userName = document.getElementById("user_name");
-const userAddress = document.getElementById("user_address");
-
-
-
 inputs.forEach((inputs) => {
     inputs.addEventListener("focus", (event) => {
         event.target.style.border = "3px solid purple";
@@ -21,7 +16,7 @@ inputs.forEach((inputs) => {
     });
 });
 
-function checkValue (){
+function checkValue() {
     inputs.forEach((inputs) => {
         const value = window.localStorage.getItem(inputs.name)
         if (inputs.type === "checkbox") {
